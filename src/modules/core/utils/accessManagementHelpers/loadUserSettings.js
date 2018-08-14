57cfbc45872ebe97bd0e48b743f00d13a6dfde54
vip <mostafa.ali.mohamed@gmail.com>
2018-08-14 09:47:23 +0200

    - User Settings Query Removed

--- src/modules/core/utils/accessManagementHelpers/loadUserSettings.js
+++ src/modules/core/utils/accessManagementHelpers/loadUserSettings.js
@@ -1,20 +1,4 @@
-import { fetchQuery, graphql } from 'relay-runtime';
-
-const query = graphql`
-  query loadUserSettingsQuery {
-    system_configuration_settings {
-      current_location_country_code
-    }
-  }
-`;
-
-export default (environment, callback) => {
-  fetchQuery(environment, query)
-    .then((data) => {
-      if (data) {
-        callback(data.system_configuration_settings);
-      } else {
-        throw new Error("Couldn't load user's configuration settings");
-      }
-    });
+export default () => {
+  // TODO : Check the changes in the commit with message
+  //      "User Settings query Removed", and find a generic solution
 };
