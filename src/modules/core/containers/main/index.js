2575c6aa38b3360b7a1f67f81c4f43264ac57173
vip <mostafa.ali.mohamed@gmail.com>
2018-08-19 19:05:32 +0200

    Support non-rendering creation of the App Components

--- src/modules/core/containers/main/index.js
+++ src/modules/core/containers/main/index.js
@@ -20,46 +20,60 @@ import DefaultTheme from './defaultTheme';
 import registerServiceWorker from './registerServiceWorker';
 import i18nextLoader from './i18n'; // initialized i18next instance
 
-let i18next = null;
-
 const portalEndPoint = process.env.REACT_APP_GRAPHQL_ENDPOINT;
 assert(portalEndPoint, "Backend endpoint isn't set correctly, call the npm build (or start), as follows : 'REACT_APP_GRAPHQL_ENDPOINT=http://portal.ayk-dev.badrit.com/graphql npm start''");
 
-const renderApp = (AppComponent, theme, modules, environment) => {
-  ReactDOM.render(
-    <Router>
-      <RelayInitializer.Context.Provider value={environment}>
-        <ModulesLoader.Context.Provider value={modules} >
-          <I18nextProvider i18n={i18next}>
-            <ThemeProvider theme={theme}>
-              <UserInfoProvider>
-                <ModalTrackerProvider>
-                  <AppComponent />
-                </ModalTrackerProvider>
-              </UserInfoProvider>
-            </ThemeProvider>
-          </I18nextProvider>
-        </ModulesLoader.Context.Provider>
-      </RelayInitializer.Context.Provider>
-    </Router>,
-    document.getElementById('root'),
-  );
-  registerServiceWorker();
+const renderApp = (AppComponent, theme, modules, environment, i18next) => (
+  <Router>
+    <RelayInitializer.Context.Provider value={environment}>
+      <ModulesLoader.Context.Provider value={modules} >
+        <I18nextProvider i18n={i18next}>
+          <ThemeProvider theme={theme}>
+            <UserInfoProvider>
+              <ModalTrackerProvider>
+                {AppComponent}
+              </ModalTrackerProvider>
+            </UserInfoProvider>
+          </ThemeProvider>
+        </I18nextProvider>
+      </ModulesLoader.Context.Provider>
+    </RelayInitializer.Context.Provider>
+  </Router>
+);
+
+const createApp = (modulesConfig, AppComponent = null, theme = null, callback) => {
+  const i18next = i18nextLoader.load((err) => {
+    if (err) throw (err);
+
+    const environment = RelayInitializer.init(portalEndPoint);
+    const modules = modulesConfig ? ModulesLoader.loadModules(modulesConfig) : [];
+
+    const appRoot = renderApp(
+      AppComponent || <DefaultLayout />,
+      theme || DefaultTheme,
+      modules,
+      environment,
+      i18next,
+    );
+
+    callback(appRoot);
+  });
 };
 
 export default {
-  renderApp: (modulesConfig, AppComponent, theme) => {
-    i18next = i18nextLoader.load((err) => {
-      if (err) return console.error(err);
-      const environment = RelayInitializer.init(portalEndPoint);
-      const modules = modulesConfig ? ModulesLoader.loadModules(modulesConfig) : [];
-      renderApp(
-        AppComponent || DefaultLayout,
-        theme || DefaultTheme,
-        modules,
-        environment,
-      );
-      return null;
-    });
+  createApp,
+  renderApp: (modulesConfig, AppComponent = null, theme = null) => {
+    createApp(
+      modulesConfig,
+      AppComponent,
+      theme,
+      (appRoot) => {
+        ReactDOM.render(
+          appRoot,
+          document.getElementById('root'),
+        );
+        registerServiceWorker();
+      },
+    );
   },
 };
