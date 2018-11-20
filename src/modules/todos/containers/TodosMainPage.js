c76ca09affacff976768a44a9fcfa66e4dfb13e3
Sarah.eldafrawy <sarah.eldafrawy257@gmail.com>
2018-11-20 09:37:08 +0200

    updates

--- src/modules/todos/containers/TodosMainPage.js
+++ src/modules/todos/containers/TodosMainPage.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { login } from '~/modules/todos/Auth';
 
 const TodosPage = (props) => {
   login(props);
   return (
-    <Link to="/todos/list">
-      Click if you are not redirected automatically
-    </Link>
+    <React.Fragment />
   );
 };
 
